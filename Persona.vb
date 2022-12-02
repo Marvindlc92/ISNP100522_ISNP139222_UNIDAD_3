30e29d94b346146266306c45fb6a9d60c3c4c1c1
F De la Cruz Diaz <F De la Cruz Diaz@DESKTOP-46LL3Q2>
2022-12-02 13:18:35 -0600

    Add project files.

---
+++ Persona.vb
@@ -0,0 +1,16 @@
+﻿Public Class Persona
+
+    Private nombre As String
+    Private edad As Integer
+
+    Public Property nombrePersona As String
+
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+            nombre = value
+        End Set
+    End Property
+
+End Class
